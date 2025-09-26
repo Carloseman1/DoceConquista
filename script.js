70bfb31803e064c9dae99b9cef25cbe44975c673
Carloseman1 <emanuelcarlos030@gmail.com>
2025-09-26 00:38:13 -0300

    Alteração

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-const mensagem = "Oii, gostaria de saber mais sobre os seus produtos! ❤️"
-const link = "https://wa.me/554791221007?text=" + encodeURIComponent(mensagem)
-for (
-  let i = 0;
-  i < document.getElementsByClassName("link-whatsapp").length;
-  i++
-) {
-  document.getElementsByClassName("link-whatsapp")[i].href = link
-}
-
 const clickNossaHistoria = () => {
   alert("Em breve!")
 }
@@ -17,10 +7,7 @@ const clickCardario = () => {
 }
 
 const clickWhatsApp = () => {
-  const mensagem =
-    "Oii, gostaria de saber mais sobre os seus produtos! ;) &#10084;"
-  const msg =
-    "Olá, gostaria de encomendar alfajores!! Sendo eles \n 8 de chocolate, 8 de ninho com nutella e 8 de paçoca. &#10084;"
+  const mensagem = "Oii, gostaria de saber mais sobre os seus produtos! ;) "
   const link = "https://wa.me/554791221007?text=" + mensagem
   window.open(link, "_blank")
 }
